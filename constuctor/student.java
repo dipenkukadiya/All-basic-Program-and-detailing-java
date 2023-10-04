91a5e3366f6c6a3e26cc5961b1a21a46ea5f5035
dipenkukadiya <dipenkukadiya@gmail.com>
2023-10-04 13:09:39 +0530

    datatype,variable,constuctor initialiize

--- constuctor/student.java
+++ constuctor/student.java
@@ -1,18 +1,39 @@
 package constuctor;
 
+class person {
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        System.out.println("this one first");
+        student s = new student("dipen", 12, 1, 100);
+    }
+
+    public person(String name, int age) {
+        System.out.println("hello dipen");
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
 public class student extends person {
     int rollno;
     int marks;
+    
 
-    public student(String name, int age , int rollno , int marks) {
+    public student(String name, int age, int rollno, int marks) {
         super(name, age);
-        this.age=age;
-        this.name=name;
-        this.rollno=rollno;
-        this.marks= marks;
-        System.out.println("hello i am");
+        System.out.println(".................");
+        this.age = age;
+        this.name = name;
+        this.rollno = rollno;
+        this.marks = marks;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(rollno);
+        System.out.println(marks);
 
-        
     }
-  
+
 }
