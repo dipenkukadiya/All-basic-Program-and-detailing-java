81a74ace6bbcce42b1194d01e53f5fe826fd8bcc
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-05 16:40:58 +0530

    5thoct

---
+++ oops/classobj.java
@@ -0,0 +1,10 @@
+package oops;
+public class classobj{
+    public static void main(String[] args) {
+        int x= 5;
+        int y=7;
+        int z= x++ + ++y - --x +y--;
+        System.out.println(z);
+    }
+}
+
