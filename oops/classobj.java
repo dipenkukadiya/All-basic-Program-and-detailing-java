79d6e723ae1328098ad4bb16d48ce7f5f14e7587
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-09 18:14:52 +0530

    jh

--- oops/classobj.java
+++ oops/classobj.java
@@ -1,10 +1,18 @@
 package oops;
-public class classobj{ 
+
+public class classobj {
     public static void main(String[] args) {
-        int x= 5;
-        int y=7;
-        int z= x++ + ++y - --x +y--;
-        System.out.println(z);
+        System.out.println("class object");
+
     }
+
 }
 
+class hello extends classobj {
+
+    hello(){
+        System.out.println("sadasda");
+    }
+    classobj cl = new classobj();
+
+}
