66d22587d110e74dc9c08c9b4a68f1a97f12f8a6
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-10 17:46:26 +0530

    overloaded

---
+++ oops/overloaded4.java
@@ -0,0 +1,34 @@
+package oops;
+
+public class overloaded4{
+     public static void main(String[] args) {
+        test t  =  new test();
+        animal a = new animal();
+        t.m1(a);
+        monkey m = new monkey();
+        t.m1(m);
+        animal a1 = new monkey();
+        t.m1(a1);
+      
+     }
+
+}
+class animal{
+}
+class monkey extends animal{
+}
+class test{
+
+    public void m1(animal a){
+        System.out.println("animal version");
+    }
+    public void m1(monkey m){
+        System.out.println("monkey verison");
+
+    }
+
+}
+
+
+//string and string methode are both cancall null object if 
+// call t.m1(null) then we get compile time error like m1 is ambiguous methode
