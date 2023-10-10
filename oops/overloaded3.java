5dde2f582a91567b6643ddc6f44d095c933ac433
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-10 17:25:16 +0530

    int ... methde

---
+++ oops/overloaded3.java
@@ -0,0 +1,30 @@
+package oops;
+
+public class overloaded3{
+     public static void main(String[] args) {
+        test t  =  new test();
+        t.m1();
+        t.m1(10,20);
+        t.m1(102);
+       
+     }
+
+}
+class test{
+    public void m1(int x){
+        System.out.println("genere methode");
+    }
+    public void m1(int... x){
+        System.out.println("new verison");
+
+    }
+    public void m1(StringBuffer sb){
+        System.out.println("stingbuufer");
+    }
+
+
+
+}
+
+
+//genere methode has least priority
