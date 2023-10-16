5015dbec55aa0d8a2166fe3a52c2e1fb446d5752
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-16 16:21:36 +0530

    lambada program

---
+++ oops/lambda.java
@@ -0,0 +1,13 @@
+package oops;
+
+interface tst{
+    void print();
+}
+public class lambda {
+        public static void main(String[] args) {
+        full(()->System.out.println("hello"));
+    }
+    
+    static void full(tst t){t.print();}
+
+}
