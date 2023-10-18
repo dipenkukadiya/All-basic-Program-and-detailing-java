6bfcea7b338a7e9d6bffd1b48d6ccb6f5274e9a9
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-18 12:28:43 +0530

    changes in de

--- exception_program/default_exe.java
+++ exception_program/default_exe.java
@@ -2,13 +2,15 @@ package exception_program;
 class default_exe{
 public static void main(String[] args) {
     dostuff();
+    System.out.println(10/0);
 }
 public static void dostuff() {
     domorestuff();
+    System.out.println("hi");
 
 }
 public static void domorestuff() {
-    System.out.println(10/0);
+    System.out.println("hello");
     
 }
 
