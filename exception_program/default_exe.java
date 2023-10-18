51457d9a9d60931346bab7f97e86177277f90d52
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-18 12:11:16 +0530

    default exception Arithmetic

---
+++ exception_program/default_exe.java
@@ -0,0 +1,17 @@
+package exception_program;
+class default_exe{
+public static void main(String[] args) {
+    dostuff();
+}
+public static void dostuff() {
+    domorestuff();
+
+}
+public static void domorestuff() {
+    System.out.println(10/0);
+    
+}
+
+
+}
+// Arithmathic exeception give bydefault
