ce85a64937d994a9af8225f469f73e5a748a2bee
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-18 15:00:21 +0530

    try catch

---
+++ exception_program/try_catch.java
@@ -0,0 +1,25 @@
+package exception_program;
+
+class try_catch {
+
+    public static void main(String[] args) {
+        System.out.println("statement 1");
+
+        try {
+            System.out.println(10 / 0);
+            System.out.println("stt2");
+        } catch (ArithmeticException e) {
+
+            try{
+                System.out.println(10 / 0);
+
+                System.out.println("stt4");
+            }
+            catch(ArithmeticException a){
+                System.out.println("hey");
+            }
+        }
+
+        System.out.println("statement 3");
+    }
+}
