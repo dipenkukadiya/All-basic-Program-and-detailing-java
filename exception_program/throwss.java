b6dadf713b6420374c142a8afd7d2144da5ad3cc
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-19 12:35:03 +0530

    changes in throws

--- exception_program/throwss.java
+++ exception_program/throwss.java
@@ -4,12 +4,14 @@ public class throwss {
 
     public static void main(String[] args) {
 
-        System.out.println(10 / 0);
+        // System.out.println(10 / 0);
         // output of this-->
         // //dipen@gtcsys-dipen:~/Dipenfiles/learning/exception_program$ java
         // throwss.java
         // Exception in thread "main" java.lang.ArithmeticException: / by zero
         throw new ArithmeticException("/ byyy zero");
+        //   System.out.println("heloo"); //if we do this then we got error
+                                        //like unreachable statement.
         // output of this -->
         // dipen@gtcsys-dipen:~/Dipenfiles/learning/exception_program$ java
         // throwss.java
