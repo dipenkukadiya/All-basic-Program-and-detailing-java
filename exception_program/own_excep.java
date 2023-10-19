0387cd7ab1fdb354555fc5bb207ea7e944bc3c88
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-19 16:39:59 +0530

    some fles

---
+++ exception_program/own_excep.java
@@ -0,0 +1,8 @@
+package exception_program;
+
+public class own_excep extends RuntimeException {
+    
+public static void main(String[] args) {
+    throw new own_excep();
+}
+}
---
+++ exception_program/throws_keyword.java
@@ -0,0 +1,16 @@
+package exception_program;
+
+public class throws_keyword {
+    public static void main(String[] args) throws InterruptedException   {
+        Thread.sleep(10000);
+        
+    }
+    }
+ // where we declare sleep method be chance to interrupt in code show we 
+ // must  be throws interruptedException in methods
+
+
+
+
+//  we can use throws keyword for constructor and method not for class
+//for thro
