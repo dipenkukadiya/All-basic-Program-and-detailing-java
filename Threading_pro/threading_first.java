5ea7bf79bcbf8cbb695fb9cf2a22bc316bb8e0f5
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-25 10:56:14 +0530

    Threading concept

---
+++ Threading_pro/threading_first.java
@@ -0,0 +1,21 @@
+package Threading_pro;
+
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("CT");
+        }
+    }
+}
+
+class threading_first {
+    public static void main(String[] args) {
+
+        MyThread t = new MyThread();
+        t.start();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("M_T");
+        }
+    }
+
+}
