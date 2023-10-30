3b128bc56075fdc8f1506deafdef85b3793671ab
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-30 12:59:57 +0530

    thread gropu UR DAEMON O RNOT

---
+++ Threading_pro/Threadgroupdemo.java
@@ -0,0 +1,11 @@
+package Threading_pro;
+
+public class Threadgroupdemo {
+    public static void main(String[] args) {
+        ThreadGroup system = Thread.currentThread().getThreadGroup().getParent();
+        Thread[] t = new Thread[system.activeCount()];
+    system.enumerate(t);
+for( Thread t1 : t){
+    System.out.println(t1.getName()+"...."+t1.isDaemon());}
+}    
+}
